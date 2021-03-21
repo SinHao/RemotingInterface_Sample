7bef4a5c72bd4f8bf03300a5f0b571119636ac8c
SinHao <sos80065@yahoo.com.tw>
2021-03-21 15:12:03 +0800

    first commit

---
+++ IMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemotingInterface
+{
+    public interface IMethods
+    {
+        void Method1(string value1);
+        string Method2(string value1);
+    }
+}
